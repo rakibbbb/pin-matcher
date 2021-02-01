7f39853d27012921015eb04ce51765b3e6bbd4ac
Md. Rakibuzzaman <rakib141746@gmail.com>
2021-02-01 20:59:09 +0600

    Pin Matcher

---
+++ js/script.js
@@ -0,0 +1,65 @@
+//Generate Pin
+function getPin(){
+    const random = Math.random()*10000;
+    const pin = (random + '').split('.')[0];
+
+    if(pin.length === 4){
+        return pin;
+    }
+    else{
+        return getPin();
+    }
+};
+
+//Display Generated Pin
+function generatePin(){
+    const showPin = document.getElementById('show-pin');
+    showPin.value = getPin();
+}
+
+//Handle Button Event
+const buttonContainer = document.getElementById('digits-container');
+buttonContainer.addEventListener('click', function(event){
+    const digit = event.target.innerText;
+    if(isNaN(digit)){
+        //handle clear
+        if(digit === 'C')
+        {
+            const typeInput = document.getElementById('typed-digits');
+            typeInput.value = '';
+        }
+        //handle back
+    }
+    else{
+        const typeInput = document.getElementById('typed-digits');
+        typeInput.value = typeInput.value + digit;
+    }
+});
+
+
+//Verify Pin
+function verifyPin(){
+    const pin = document.getElementById('show-pin').value;
+    const typedPin = document.getElementById('typed-digits').value;
+    if(pin === typedPin){
+        // const pinMatched = document.getElementById('matched');
+        // pinMatched.style.display = 'block';
+        // const pinNotMatched = document.getElementById('not-matched');
+        // pinNotMatched.style.display = "none";
+        displayMatchResult('block', 'none');
+    }
+    else{
+        // const pinMatched = document.getElementById('matched');
+        // pinMatched.style.display = 'none';
+        // const pinNotMatched = document.getElementById('not-matched');
+        // pinNotMatched.style.display = "block";
+        displayMatchResult('none', 'block');
+    }
+}
+
+function displayMatchResult(correctStatus, incorrectStatus){
+    const pinMatched = document.getElementById('matched');
+    pinMatched.style.display = correctStatus;
+    const pinNotMatched = document.getElementById('not-matched');
+    pinNotMatched.style.display = incorrectStatus;
+}
